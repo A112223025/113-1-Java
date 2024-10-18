90b27b397cc73e0a5fb338264201a9c680c234a5
A112223025 <a112223025@mail.shu.edu.tw>
2024-10-18 12:06:44 +0800

    113-10-18

---
+++ Encapulation3/src/CardApp.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import edu.shu.Angela.*;
+
+public class CardApp {
+
+    public static void main(String[] args) {
+        CashCard[] cards ={
+                new CashCard("A001", 500, 0);
+                new CashCard("A002", 300, 0);
+                new CashCard("A003", 1000, 1);
+        };
+        Scanner inout = Scanner(System.in);
+        for(CashCard Card : Cards){
+            System.out.printf("")
+        }
+
+
+    }
+}
---
+++ HW1/src/Time1.java
@@ -0,0 +1,11 @@
+public class Time1
+//十(hour)
+// 分(minute)
+//秒(second)
+
+//方法:建構子(設定時分秒的初始直，需檢查資料是否格式正確)
+//方法:tick(時間增加一秒)
+//方法:是需要加入get/set
+//方法:printUniversal(以24小時制輸出完整時間)
+//方法:printStandard(以12小時制輸出完整時間)
+}
